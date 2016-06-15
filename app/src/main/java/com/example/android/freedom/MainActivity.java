e0301463777932581911cadbb55292cd86031287
Bevan Small <bevan.r.small@gmail.com>
2016-06-15 15:06:38 +0100

    Added a results textview as a placeholder for cards. disappears, appears and updates text depending on the region spinner selection

--- app/src/main/java/com/example/android/freedom/MainActivity.java
+++ app/src/main/java/com/example/android/freedom/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.android.freedom;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +18,45 @@ public class MainActivity extends AppCompatActivity {
 
         spRegions = (Spinner) findViewById(R.id.regions_spinner);
 
+
+        // create listener to change result text when the spinner option is changed
+        spRegions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                makeResultVisible();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                makeResultInvisible();
+            }
+        });
+
+
     }
 
+    // Makes the result view viewable if the correct region is selected
+    // Changes result text according to region
+    public void makeResultVisible() {
+        if (spRegions.getSelectedItem().toString().equals("Auckland")) {
+            TextView tv = (TextView) findViewById(R.id.spinner_result);
+            tv.setText(R.string.result_auckland);
+            tv.setVisibility(View.VISIBLE);
+        } else if(spRegions.getSelectedItem().toString().equals("Canterbury")){
+            TextView tv = (TextView) findViewById(R.id.spinner_result);
+            tv.setText(R.string.result_canterbury);
+            tv.setVisibility(View.VISIBLE);
+        } else {
+            makeResultInvisible();
+        }
+    }
 
-
+    // makes the spinner result text invisible
+    public void makeResultInvisible() {
+        TextView tv = (TextView) findViewById(R.id.spinner_result);
+        tv.setVisibility(View.INVISIBLE);
+    }
 }
+
+
+
